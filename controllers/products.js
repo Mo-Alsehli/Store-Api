3480d297cb43645a96e802a744aa219b84813786
Mo-Alsehli <mohamed.m.alsehli@gmail.com>
2022-08-02 14:56:05 +0200

    applying sorting functionality

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = (req, res) => {
 const getAllProducts = async (req, res) => {
   // Filtering Products
 
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -18,7 +18,15 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  const products = await Products.find(queryObject);
+
+  let result = Products.find(queryObject);
+  if (sort) {
+    let sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
