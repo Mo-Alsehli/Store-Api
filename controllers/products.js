5beedaeb0f6e3babb56d5ad2993910f570b8a1d7
Mo-Alsehli <mohamed.m.alsehli@gmail.com>
2022-08-02 15:54:11 +0200

    Add Pagination

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = (req, res) => {
 const getAllProducts = async (req, res) => {
   // Filtering Products
 
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -20,12 +20,27 @@ const getAllProducts = async (req, res) => {
   }
 
   let result = Products.find(queryObject);
+
+  // Sorting Results
   if (sort) {
     let sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
+
+  // Selecting Specific Fileds
+  if (fields) {
+    let fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+
+  // Pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
